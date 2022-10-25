b0e87090df1b1c3eee78cbbd21b7c218b7d801f5
sobadrush <sobadrush@icloud.com>
2022-10-26 01:03:16 +0800

    測試新增 HelloWorldController.cs

---
+++ Todo/Controllers/HelloWorldController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        private static string WELCOME_MSG = "Hello World";
+        
+        // GET: api/HelloWorld
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { WELCOME_MSG.Split(" ")[0], WELCOME_MSG.Split(" ")[1] };
+        }
+
+        // GET: api/HelloWorld/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return WELCOME_MSG;
+        }
+
+        // POST: api/HelloWorld
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/HelloWorld/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/HelloWorld/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
