3a1d174c862a81fbc4e19d84a39228cf9d96a534
sobadrush <sobadrush@icloud.com>
2022-10-31 18:12:12 +0800

    將DB連線字串移到 appsettings.json，並修改 Program.cs 讀取之

--- Todo/Program.cs
+++ Todo/Program.cs
@@ -1,3 +1,7 @@
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Todo.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// --- Start Of: DB 連線設定 ---
+builder.Services.AddDbContext<MyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDatabaseConnString"));
+});
+// --- End Of: DB 連線設定 ---
 
 var app = builder.Build();
 
