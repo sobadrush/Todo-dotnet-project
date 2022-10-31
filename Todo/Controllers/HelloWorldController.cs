dd6dd83c7fcdf32203ea380acab7a90032450684
sobadrush <sobadrush@icloud.com>
2022-10-31 18:24:10 +0800

    重構: 將 namespace Todo.Controllers; 移到最上

--- Todo/Controllers/HelloWorldController.cs
+++ Todo/Controllers/HelloWorldController.cs
@@ -1,43 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Todo.Controllers
+namespace Todo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HelloWorldController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class HelloWorldController : ControllerBase
+    private static string WELCOME_MSG = "Hello World";
+    
+    // GET: api/HelloWorld
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        private static string WELCOME_MSG = "Hello World";
-        
-        // GET: api/HelloWorld
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { WELCOME_MSG.Split(" ")[0], WELCOME_MSG.Split(" ")[1] };
-        }
+        return new string[] { WELCOME_MSG.Split(" ")[0], WELCOME_MSG.Split(" ")[1] };
+    }
 
-        // GET: api/HelloWorld/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return WELCOME_MSG;
-        }
+    // GET: api/HelloWorld/5
+    [HttpGet("{id}", Name = "Get")]
+    public string Get(int id)
+    {
+        return WELCOME_MSG;
+    }
 
-        // POST: api/HelloWorld
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    // POST: api/HelloWorld
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
 
-        // PUT: api/HelloWorld/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    // PUT: api/HelloWorld/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
 
-        // DELETE: api/HelloWorld/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // DELETE: api/HelloWorld/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
